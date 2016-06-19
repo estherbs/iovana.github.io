52c7239dd56c318b33710c70583165b13005136e
iovana <ip4g14@soton.ac.uk>
2016-06-19 19:40:38 +0100

    Day 2

--- controller.js
+++ controller.js
@@ -1,4 +1,33 @@
 /**
  * Created by Iovana on 18/06/2016.
  */
-    
+
+var slided = (function(){
+    var slid = false;
+    return {
+        get : function() {
+            return slid;
+        },
+        set : function(b) {
+            slid = b;
+        }
+    };
+})();
+
+function pageDown(event) {
+    var key = event.keyCode;
+    if(key == 13 && !slided.get()) {
+       var height = "-=" + $('#top').height() + "px";
+        $('#top').animate({
+            'top' : height
+        }, 1000);
+        $('#bottom').animate({
+            'top' : height
+        }, 1000);
+        slided.set(true);
+    }
+}
+
+    $(document).ready(function(){
+        
+    });
