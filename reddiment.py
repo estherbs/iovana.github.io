f0e8169a82f3def1ba8f14af028dac7b8477e442
iovana <ip4g14@soton.ac.uk>
2016-06-27 18:36:49 +0100

    FinalCommit

--- reddiment.py
+++ reddiment.py
@@ -12,10 +12,11 @@ def index():
     if request.method == 'POST':
         form_data = request.form['url']
         arr = check_url(form_data, 5)
-        return render_template('main.html', slide="url is valid", pos1=arr[0], pos2=arr[1], pos3=arr[2], pos4=arr[3],
+        return render_template('main.html', slide="url is valid",
+                               pos1=arr[0], pos2=arr[1], pos3=arr[2], pos4=arr[3],
                                pos5=arr[4], pos6=arr[5], pos7=arr[6], pos8=arr[7], pos9=arr[8], pos10=arr[9],
                                neg1=arr[10], neg2=arr[11], neg3=arr[12], neg4=arr[13], neg5=arr[14], neg6=arr[15],
-                               neg7=arr[16], neg8=arr[17], neg9=arr[18], neg10=arr[19])
+                               neg7=arr[16], neg8=arr[17], neg9=arr[18], neg10=arr[19], percentage=arr[20])
     return render_template('main.html', slide="")
 
 def parse_page(name):
@@ -53,11 +54,26 @@ def hashmapfunction(stringarr):
             negativehashmap[word] += 1
     sorted_positivehashmap = sorted(positivehashmap.items(), key = operator.itemgetter(1), reverse=True)
     sorted_negativehashmap = sorted(negativehashmap.items(), key = operator.itemgetter(1), reverse=True)
+    total_positive = total(sorted_positivehashmap)
+    total_negative = total(sorted_negativehashmap)
+    total_all = total_positive + total_negative
     toptenpos = topten(sorted_positivehashmap)
     toptenneg = topten(sorted_negativehashmap)
     sortedwords = toptenpos + toptenneg
-    print(sortedwords)
+    percentage = (-200 / total_all * total_positive) + 100;
+    sortedwords.append(str(round(percentage, 2)))
     return sortedwords
+
+
+def total(sorted_list):
+    i = 0
+    total_val = 0
+    while i < 10:
+        temp_tuple = sorted_list[i]
+        total_val += temp_tuple[1]
+        i += 1
+    return total_val
+
 
 def topten(arr):
     i = 0
@@ -65,7 +81,10 @@ def topten(arr):
     while i < 10:
         if len(arr) > i:
             mypair = arr[i]
-            finalarr.append(str(mypair[1]) + " - " + mypair[0].capitalize())
+            if mypair[1] < 10:
+                finalarr.append(" " + str(mypair[1]) + " - " + mypair[0].capitalize())
+            else:
+                finalarr.append(str(mypair[1]) + " - " + mypair[0].capitalize())
         else:
             finalarr.append("N/A")
         i += 1
--- static/js/controller.js
+++ static/js/controller.js
@@ -32,6 +32,15 @@ function checkUrlEntered(){
     if(urlEntered.html()==="url is valid"){
         pageDown();
     }
+    var percent = $('#percentage_bar').html();
+    // percent = (percent.trim) ? percent.trim() : percent.replace(/^\s+/,'');
+    if(percent != ''){
+        var x = parseFloat(percent);
+        var yr = 255 + (0.875 * x) - (0.01575 * Math.pow(x, 2));
+        var yg = 140 - (0.75 * x) - (0.0065 * Math.pow(x, 2));
+        var mycolor = "rgb(" + parseInt(yr) + "," + parseInt(yg) + ",0)";
+        $('#bottom').css("backgroundColor", mycolor);
+    }
 }
 
     $(document).ready(function(){
